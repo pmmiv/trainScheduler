d47774b3e928ceb6164a3689b93a37d4cdf07de9
PMMIV <pmmilamiv@gmail.com>
2018-02-03 10:15:13 -0600

    made some jumbotron changes, added form validation.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,51 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCheowTlssv5Oy6qR1bvrSuXmldwwF3VtM",
-    authDomain: "train-scheduler-4005c.firebaseapp.com",
-    databaseURL: "https://train-scheduler-4005c.firebaseio.com",
-    projectId: "train-scheduler-4005c",
-    storageBucket: "train-scheduler-4005c.appspot.com",
-    messagingSenderId: "594644763872"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCheowTlssv5Oy6qR1bvrSuXmldwwF3VtM",
+  authDomain: "train-scheduler-4005c.firebaseapp.com",
+  databaseURL: "https://train-scheduler-4005c.firebaseio.com",
+  projectId: "train-scheduler-4005c",
+  storageBucket: "train-scheduler-4005c.appspot.com",
+  messagingSenderId: "594644763872"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  // on click pull the whole form data and push it to the firebase
-  $('#submit').click(function(event){
+function timeUpdate () {
+  $('#currentTime').text(moment().format('hh:mm a'));
+};
+
+function validateForm () {
+  var a = $('#tName').val().trim();
+  var b = $('#tDest').val().trim();
+  var c = $('#tFirstTime').val().trim();
+  var d = $('#tFreq').val().trim();
+  if (a == "") {
+    alert("The train must have a name.");
+    return false;
+  } else if (b == "") {
+    alert("The train must have a destination.");
+    return false;
+  } else if (c == "") {
+    alert("The train needs a start time.");
+    return false;
+  } else if (d == "") {
+    alert("The train must have an arrival frequency.");
+    return false;
+  } else {
+    return true;
+  }};
+
+$( document ).ready(function() {
+  timeUpdate();
+  setInterval(timeUpdate, 60000);
+});
+
+// on click pull the whole form data and push it to the firebase
+$('#submit').click(function(event){
+  if (validateForm()) {
+
+    timeUpdate();
     event.preventDefault();
     var name = $('#tName').val().trim();
     var dest = $('#tDest').val().trim();
@@ -29,50 +62,36 @@
     $('#tDest').val("");
     $('#tFirstTime').val("");
     $('#tFreq').val("");
-  });
+  }
+});
 
-  database.ref("/trains").on("child_added", function(childSnapshot) {
-    var wholeEntry = $('<tr>')
-    var entryName = $('<td>');
-    entryName.text(childSnapshot.val().name);
-    var entryDest = $('<td>');
-    entryDest.text(childSnapshot.val().dest);
-    var entryTime = $('<td>');
-    entryTime.text(childSnapshot.val().firstTime);
-    var entryFreq = $('<td>');
-    entryFreq.text(childSnapshot.val().freq);
-    wholeEntry.append(entryName, entryDest, entryTime, entryFreq);
-    $('#trainSchedule').append(wholeEntry);
-  })
+// just here to clear database
+$('#clear').click(function(event){
+  database.ref("/trains").remove();
+  console.log("Database Clear")
+});
 
+database.ref("/trains").on("child_added", function(childSnapshot) {
+  var wholeEntry = $('<tr>')
+  var entryName = $('<td>');
+  entryName.text(childSnapshot.val().name);
+  var entryDest = $('<td>');
+  entryDest.text(childSnapshot.val().dest);
+  var entryFreq = $('<td>');
+  entryFreq.text(childSnapshot.val().freq + " minutes");
 
-// -----THIS IS AN EXAMPLE TO CALCULATE THE TRAIN ARRIVAL DIFFERENCE ----------
-    // // Assumptions
-    // var tFrequency = 3;
+  var entryFT = moment(childSnapshot.val().firstTime, 'HH:mm').format('hh:mm');
+  var entryFTConverted = moment(entryFT, "hh:mm").subtract(1, "years");
+  var diffTime = moment().diff(moment(entryFTConverted), "minutes");
+  var tRemainder = diffTime % (childSnapshot.val().freq);
+  var tMinutesTillTrain = (childSnapshot.val().freq) - tRemainder;
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
-    // // Time is 3:30 AM
-    // var firstTime = "03:30";
+  var entryNext = $('<td>');
+  entryNext.text(moment(nextTrain).format("hh:mm a"));
+  var entryMin = $('<td>');
+  entryMin.text(tMinutesTillTrain + " minutes");
 
-    // // First Time (pushed back 1 year to make sure it comes before current time)
-    // var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
-
-    // // Current Time. Can this not be accomplished without declaring a variable? moment().format("hh:mm");
-    // var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // // Difference between the times
-    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // // Time apart (remainder)
-    // var tRemainder = diffTime % tFrequency;
-    // console.log(tRemainder);
-
-    // // Minute Until Train
-    // var tMinutesTillTrain = tFrequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // // Next Train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  wholeEntry.append(entryName, entryDest, entryFreq, entryNext, entryMin);
+  $('#trainSchedule').append(wholeEntry);
+});
